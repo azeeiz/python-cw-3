fd1a627756d5650be16f06f7388eda80562ce399
azeeiz <118297146+azeeiz@users.noreply.github.com>
2022-11-30 19:52:36 +0300

    done
    done

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # write your code here
+#1
+favorite_animals = ['dog', 'cat', 'monkey', 'rabbit']
+#print(favorite_animals)
+
+#favorite_animals.remove('monkey')
+#print(favorite_animals)
+
+#2
+favorite_animals.append('donkey')
+print(favorite_animals)
+animal = []
+for animal in favorite_animals:
+    print(f'I love {animal}')
+
+#3
+numbers = [1, 2, 3, 4, 5]
+numbers_sum = 0
+for numbers in numbers:
+    numbers_sum += numbers
+    print(numbers_sum)
+
---
+++ numbers.py
@@ -0,0 +1,3 @@
+num = int(input('Put your number'))
+for count in range(1,10):
+    print(num, 'x', count, '=', num * count)
